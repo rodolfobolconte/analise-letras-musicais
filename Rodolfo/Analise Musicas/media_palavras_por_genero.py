a72e96ecf5e645aa3b9232404670c9dd5f847c7f
RodolfoBolconte <rodolfobolconte@gmail.com>
2018-05-12 21:46:34 -0300

    atualizando grafico de medias

--- Rodolfo/Analise Musicas/media_palavras_por_genero.py
+++ Rodolfo/Analise Musicas/media_palavras_por_genero.py
@@ -2,7 +2,7 @@ import os
 
 path_raiz = "../Armazenamento Musicas/musicas/"
 
-def media_palavras(genero):
+def media_palavras_por_genero(genero):
 
 	media_palavras = 0
 
@@ -10,16 +10,26 @@ def media_palavras(genero):
 
 	for caminho_musica in path_musicas:
 
-
 		arquivo_musica = open(path_raiz + genero + '/' + caminho_musica, 'r')
 
 		musica = arquivo_musica.read().replace('\n', ' ').split(' ')
 		media_palavras += len(musica)
 
-	return media_palavras / 20
+	return media_palavras / len(path_musicas)
 
 
 generos = ['funk', 'rock', 'sertanejo']
 
+medias = {}
 for genero in generos:
-	print(genero, media_palavras(genero))
+	medias[genero.upper()] = media_palavras_por_genero(genero)
+
+
+from pylab import *
+
+grafico = bar(medias.keys(), medias.values(), width=0.3, color=['red', 'black', 'green'])
+
+mng = plt.get_current_fig_manager()
+mng.window.showMaximized()
+
+show()
