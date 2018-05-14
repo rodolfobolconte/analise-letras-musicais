e0a69478988f4f6ea07b451afd6ff4b1e5e9b3e8
MiqueasGaldino <miqueasgaldino@gmail.com>
2018-05-14 14:27:46 -0300

    Add files via upload
    adicionando  código de analise de autoinformacao

---
+++ Rodolfo/Analise Musicas/auto_informacao.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 14 14:15:28 2018
+
+@author: mique
+"""
+
+def media_autoinformacao_por_genero(genero):
+    arquivo_music = open('Auto_Informacao/' + genero  + '_auto_informacao.txt' , 'r')
+    valores = []
+    for linha in arquivo_music:
+        leitura  = linha.split("\n")
+        x = float(leitura[0])
+        valores.append(x)
+    
+    valores = set(valores)
+    valores_ordenados = sorted(valores)
+    y = len(valores_ordenados)
+    maiores_valores = []
+    for i in range(1,6):
+       
+        maiores_valores.append(valores_ordenados[y-i])
+    
+    return maiores_valores
+    
+generos = ['forro', 'mpb','rock','sertanejo','funk']
+medias = {}
+for genero in generos:
+	medias[genero.upper()] = media_autoinformacao_por_genero(genero)
+print(medias)
